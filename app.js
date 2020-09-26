9647f50c1d52dc3b38f4157eb78f3825ab7129ff
Oleg Belochitsky <olegbelo8@gmail.com>
2020-09-26 16:15:46 +0300

    Added main route (not todo routes)

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+import express from "express";
 
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 const MONGODB_URL = process.env.MONGODB_URL;
 
-console.log(NODE_ENV);
-console.log(PORT);
-console.log(HOST);
-console.log(MONGODB_URL);
+const app = express();
+
+app.use("/", (req, res) => {
+  res.status(400);
+  res.end();
+});
+
+
+let testables;
+
+if (NODE_ENV in ["development", "production"]) {
+  /* listen on port */
+  app
+    .listen(PORT, () => console.log(`server running on http:${HOST}:${PORT}`))
+    .on("error", (e) => {
+      console.error(e.message);
+      throw e;
+    });
+} else if (NODE_ENV == "test") {
+  testables = { app };
+} else {
+  console.log("Bad NODE_ENV, should be one of: development/production/test");
+}
+
+export { testables };
