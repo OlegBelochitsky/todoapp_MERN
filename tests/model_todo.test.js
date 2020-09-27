12d4c66361d9d332f205fa051d69177eb15d879f
Oleg Belochitsky <olegbelo8@gmail.com>
2020-09-27 15:30:20 +0300

    Added tests for saving todo model

--- tests/model_todo.test.js
+++ tests/model_todo.test.js
@@ -1,22 +1,37 @@
-import todoModel from "./todo.js";
+import mongoose from "mongoose";
+import todoModel from "../models/todo.js";
+import testData from "./testData.json";
 
 test("Should failed when env not test ", () => {
   expect(process.env.NODE_ENV).toEqual("test");
 });
 
-describe("", () => {
-  let comment;
-
+describe("Testing todo model", () => {
   beforeAll(() => {
     mongoose.connect(process.env.MONGODB_URL);
   });
 
-  beforeEach(() => {
+  afterEach(() => {
+    return todoModel.remove({});
   });
 
-  it();
+  it("Should save a singleTodo", async () => {
+    await todoModel.saveTodo(testData.singleTodo);
+    const todo = await todoModel
+      .find({ description: testData.singleTodo.description })
+      .exec();
+    expect(todo.description).toBe(testData.singleTodo.description);
+  });
 
-  afterEach(() => {
+  it("Should save a todoList", async () => {
+    await todoModel.saveTodo(testData.todoList);
+    const todo = await todoModel
+      .find({ description: testData.todoList.description })
+      .exec();
+    expect(todo.description).toBe(testData.todoList.description);
+    expect(todo.subTodos[1].subTodos[2].description).toBe(
+      testData.todoList.subTodos[1].subTodos[2].description
+    );
   });
 
   afterAll((done) => {
