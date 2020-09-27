b2c5464b38109a117b252f8679e9e257bce2f9b6
Oleg Belochitsky <olegbelo8@gmail.com>
2020-09-27 16:53:54 +0300

    created tests for BFS

---
+++ tests/util.test.js
@@ -0,0 +1,33 @@
+import { BFS } from "../util/bfs.js";
+import testData from "./testData.json";
+
+describe("Testing async generator BFS", () => {
+  it("can scan graph with one node", async () => {
+    const generator = BFS({ data: 1 }, "sons", (node) => node.data);
+    const traverseOrder = [];
+    for await (let item of generator) {
+      traverseOrder.push(item);
+    }
+    expect(traverseOrder).toEqual([1]);
+  });
+
+  it("should scan test grap", async () => {
+    const generator = BFS(testData.graph, "sons", (node) => node.data);
+    const traverseOrder = [];
+    for await (let item of generator) {
+      traverseOrder.push(item);
+    }
+    expect(traverseOrder).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("should work on cyclic graps", async () => {
+    const cyclic = { data: 1, sons: [{ data: 2, sons: [] }] };
+    cyclic.sons[0].sons.push(cyclic);
+    const generator = BFS(cyclic, "sons", (node) => node.data);
+    const traverseOrder = [];
+    for await (let item of generator) {
+      traverseOrder.push(item);
+    }
+    expect(traverseOrder).toEqual([1, 2]);
+  });
+});
