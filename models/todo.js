e3ad30dc81ff96c354afb643f409da3faaf1c898
Oleg Belochitsky <olegbelo8@gmail.com>
2020-09-27 23:53:17 +0300

    change in plans ( for the way of saving )

--- models/todo.js
+++ models/todo.js
@@ -18,9 +18,30 @@ todoSchema.add({
 });
 
 todoSchema.statics.saveTodo = async function (todos, callback) {
-  //TODO reverse graph pointers
-  //TODO save todo starting from childern, and push id to perents 'subTodos' , return root
-
+  //TODO warp nodes in {}
+  //TODO transfer graph to adjacency list
+  // adjacency list --> {
+  //      nodeToNumber: map(key:{ node, }  , value:number),
+  //      numberToNode: map(key:{ node, } , value:number),
+  //      invertices:[ [], [], [] ], //  keeping track of the parent
+  //      outvertices:[ [],[],[] ]   //  keeping track of the children
+  //      }
+  //TODO -save todo starting from childern,
+  //     -push id to perents 'subTodos' ,
+  //     -return root
+  // for something like this:
+  // for (const node of reversedBFS) {
+  //   const objectID = saveTodo(node);
+  //   const num = nodeToNumber.get(node);
+  //   invertices[num].forEach((n) => {
+  //     if (n?.childernID) {
+  //       n.childernID.push(objectID);
+  //     } else {
+  //       n.childernID = [objectID];
+  //     }
+  //   });
+  // }
+  //
   if (callback instanceof Function) {
     return callback(location);
   } else {
