9c1ce09cca112a1c0b9aeec5139113a8d8b595f2
Oleg Belochitsky <olegbelo8@gmail.com>
2020-09-29 12:13:29 +0300

    added test to bfs to run many times on same graph

--- tests/util_bfs.test.js
+++ tests/util_bfs.test.js
@@ -33,4 +33,21 @@ describe("Testing async generator BFS", () => {
     }
     expect(traverseOrder).toEqual([1, 2]);
   });
+
+  it("can work on same graph many times", async()=>{
+
+    const generator1 = BFS(testData.graph, "sons", (node) => node.data);
+    const traverseOrder1 = [];
+    for await (let item of generator1) {
+      traverseOrder1.push(item);
+    }
+    expect(traverseOrder1).toEqual([1, 2, 3, 4, 5]);
+
+    const generator2 = BFS(testData.graph, "sons", (node) => node.data);
+    const traverseOrder2 = [];
+    for await (let item of generator2) {
+      traverseOrder2.push(item);
+    }
+    expect(traverseOrder2).toEqual([1, 2, 3, 4, 5]);
+  });
 });
