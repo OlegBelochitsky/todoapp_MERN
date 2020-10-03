6b51f4237a213bdc44d452a3f6c862ddd8ab31c8
Oleg Belochitsky <olegbelo8@gmail.com>
2020-10-03 12:27:36 +0300

    combined the route tests to one tet suite

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,4 +1,5 @@
 import supertest from "supertest";
+import mongoose from "mongoose";
 import todoModel from "../models/todo.js";
 import testData from "./testData.json";
 import { testables } from "../app.js";
@@ -17,7 +18,27 @@ describe("GET /", () => {
   });
 });
 
+describe("POST /", () => {
+  it("responds with 404", (done) => {
+    request
+      .post("/")
+      .send({ name: "john" })
+      .expect(404)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
 describe("GET /todo", () => {
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
   afterEach((done) => {
     todoModel.deleteMany({}, done);
   });
@@ -50,9 +71,20 @@ describe("GET /todo", () => {
           todo.subTodos[1].description
     ).toEqual(true);
   });
+
+  afterAll((done) => {
+    mongoose.disconnect(done);
+  });
 });
 
 describe("GET /todo/:id", () => {
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
   afterEach((done) => {
     todoModel.deleteMany({}, done);
   });
@@ -88,5 +120,67 @@ describe("GET /todo/:id", () => {
         response.body.todo.subTodos[0].description ==
           todo.subTodos[1].description
     ).toEqual(true);
+  });
+
+  afterAll((done) => {
+    mongoose.disconnect(done);
+  });
+});
+
+describe("POST /todo", () => {
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterEach((done) => {
+    todoModel.deleteMany({}, done);
+  });
+
+  it("respond status 400 when todo is bad with massage", async () => {
+    const badTodo = { notTodo: "not a todo" };
+    const res = await request.post("/todo").set("Accept", /json/).send(badTodo);
+    expect(res.status).toEqual(404);
+    expect(res.body.massage).toEqual("invalid todo");
+  });
+
+  it("can save valid todo to db", async () => {
+    const validTodo = testData.todoList;
+    await request.post("/todo").set("Accept", /json/).send(validTodo);
+    const fromDb = await todoModel.findOne({
+      description: validTodo.description,
+    });
+    expect(fromDb.description).toEqual(validTodo.description);
+  });
+
+  it("after sucsestful save respond is status 200 with papulated todo", async () => {
+    const validTodo = testData.todoList;
+    const response = await request
+      .post("/todo")
+      .set("Accept", /json/)
+      .send(validTodo);
+    expect(response.status).toBe(200);
+    console.log(response.body);
+    expect(
+      response.body.subTodos[0].description ==
+        validTodo.subTodos[0].description ||
+        response.body.subTodos[0].description ==
+          validTodo.subTodos[1].description
+    ).toEqual(true);
+  });
+
+  it("saved todo saved with isRoot=true", async () => {
+    const validTodo = testData.todoList;
+    const res = await request
+      .post("/todo")
+      .set("Accept", /json/)
+      .send(validTodo);
+    expect(res.body.isRoot).toBe(true);
+  });
+
+  afterAll((done) => {
+    mongoose.disconnect(done);
   });
 });
